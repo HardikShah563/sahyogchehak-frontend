fc7dacf6031389d459b375a822c66c16c15d418e
HardikShah563 <hardikts@gmail.com>
2023-08-15 23:03:59 +0530

    join us

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,43 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import ContactUs from "../components/ContactUs";
+import "../style/home.css";
+
+import { donate, volunteer, partner } from "../res";
 
 export default function Home() {
     return (
         <>
             <Navbar />
             <div>
-                <h1>Home Page</h1>
+                {/* <h1 className="txt-ctr">Home Page</h1> */}
+
+                <div>
+                    <h1 className="title txt-ctr">
+                        Join Us
+                    </h1>
+                    <div className="join-us flex">
+                        <div className="join-div">
+                            <div className="join-icon flex">
+                                <img src={donate} alt="donate icon" />
+                            </div>
+                            <h1 className="txt-ctr">Donate</h1>
+                        </div>
+                        <div className="join-div">
+                            <div className="join-icon flex">
+                                <img src={volunteer} alt="donate icon" />
+                            </div>
+                            <h1 className="txt-ctr">Volunteer Us</h1>
+                        </div>
+                        <div className="join-div">
+                            <div className="join-icon flex">
+                                <img src={partner} alt="donate icon" />
+                            </div>
+                            <h1 className="txt-ctr">Partner with us</h1>
+                        </div>
+                    </div>
+                </div>
+
                 <ContactUs />
             </div>
             <Footer />
---
+++ src/res/index.js
@@ -0,0 +1,23 @@
+import donate from "./donate.png";
+import volunteer from "./volunteer.png";
+import partner from "./partner.png"
+import contactBook from "./icon-contact-book.png";
+import contactMail from "./icon-contact-mail.png";
+import facebook from "./icon-facebook.png";
+import insta from "./icon-insta.png";
+import twitter from "./icon-twitter.png";
+import sahyogLogoSmall from "./logo-sahyog-small.png";
+import sahyogLogo from "./logo-sahyog.png";
+
+export {
+    donate, 
+    volunteer, 
+    partner, 
+    contactBook, 
+    contactMail, 
+    facebook, 
+    insta, 
+    twitter, 
+    sahyogLogoSmall, 
+    sahyogLogo, 
+};
